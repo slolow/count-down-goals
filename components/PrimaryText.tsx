7f2091678610f561130d810bcc9ffc17845bcdc8
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-09 18:17:38 +0200

    feat: Create PrimaryText.tsx

---
+++ components/PrimaryText.tsx
@@ -0,0 +1,13 @@
+import {Text} from "react-native";
+import {useTheme} from "react-native-paper";
+import React, {ReactNode} from "react";
+
+type PrimaryTextProps = {
+  children: ReactNode
+}
+
+export const PrimaryText = ({ children }: PrimaryTextProps) => {
+  const theme = useTheme()
+
+  return <Text style={{ color: theme.colors.primary}}>{children}</Text>
+}
