afb58eee70f3ab4410369bc167e0fa84f44994e2
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-09 20:22:23 +0200

    feat: Add onPress functionality to IconButton.tsx

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -3,9 +3,10 @@ import { Image, ImageSourcePropType } from "react-native";
 
 type IconButtonProps = {
   source: ImageSourcePropType | undefined;
+  onPress: () => void;
 };
 
-export const IconButton = ({ source }: IconButtonProps) => {
+export const IconButton = ({ source, onPress }: IconButtonProps) => {
   return (
     <Button
       icon={({ size, color }) => (
@@ -14,6 +15,7 @@ export const IconButton = ({ source }: IconButtonProps) => {
           style={{ width: 2 * size, height: 2 * size, tintColor: color }}
         />
       )}
+      onPress={onPress}
     >
       {""}
     </Button>
