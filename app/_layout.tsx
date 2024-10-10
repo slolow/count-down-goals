68745d6b137c2bea7eb47cddcf2dddd5f7a7ee2a
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 21:53:30 +0200

    feat: Add goalsContext and GoalsProvider to _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "react-native-get-random-values";
 import { Stack } from "expo-router";
 import { PaperProvider, ActivityIndicator } from "react-native-paper";
 import { Header } from "@/components/Header";
@@ -8,15 +9,37 @@ import { darkTheme } from "@/assets/themes/darkTheme";
 import { lightTheme } from "@/assets/themes/lightTheme";
 import { useFonts } from "expo-font";
 import { Container } from "@/components/Container";
+import { v4 as uuidv4 } from "uuid";
+import { GoalsProvider } from "@/providers/GoalProvider";
+
+type Goal = {
+  id: string;
+  content: string;
+  days: number;
+};
+
+export type Goals = Goal[];
 
 const RootLayout = () => {
   const systemColorScheme = useColorScheme();
-  const [isDarkTheme, setIsDarkTheme] = useState(systemColorScheme === "dark");
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(
+    systemColorScheme === "dark",
+  );
+  const [goals, setGoals] = useState<Goals>([
+    { id: uuidv4(), content: "live sugar free", days: 30 },
+    { id: uuidv4(), content: "play guitar for 10 minutes every day", days: 90 },
+  ]);
   const colorSchemeContext = useMemo(
     () => ({
       toggleTheme: () => {
         setIsDarkTheme((isDarkTheme) => !isDarkTheme);
       },
+    }),
+    [],
+  );
+  const goalContext = useMemo(
+    () => ({
+      getGoals: () => goals,
     }),
     [],
   );
@@ -39,19 +62,21 @@ const RootLayout = () => {
 
   return (
     <ColorSchemeProvider value={colorSchemeContext}>
-      <PaperProvider theme={theme}>
-        <Stack
-          screenOptions={{
-            header: () => <Header />,
-            contentStyle: { backgroundColor: theme.colors.background },
-          }}
-        >
-          <Stack.Screen name="index" />
-          <Stack.Screen name="SetGoal" />
-          <Stack.Screen name="SetDays" />
-          <Stack.Screen name="Goal" />
-        </Stack>
-      </PaperProvider>
+      <GoalsProvider value={goalContext}>
+        <PaperProvider theme={theme}>
+          <Stack
+            screenOptions={{
+              header: () => <Header />,
+              contentStyle: { backgroundColor: theme.colors.background },
+            }}
+          >
+            <Stack.Screen name="index" />
+            <Stack.Screen name="SetGoal" />
+            <Stack.Screen name="SetDays" />
+            <Stack.Screen name="Goal" />
+          </Stack>
+        </PaperProvider>
+      </GoalsProvider>
     </ColorSchemeProvider>
   );
 };
