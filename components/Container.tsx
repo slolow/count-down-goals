805d014a1547ff23b8dafb09d4a3e7af4205a535
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 18:27:52 +0200

    feat: Add a mode property to container to switch between top and centered

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,21 +1,25 @@
-import { View } from "react-native";
+import { Dimensions, View } from "react-native";
 import { ReactNode } from "react";
 
 type ContainerProps = {
   children: ReactNode;
   style?: Object;
+  mode?: "top" | "centered";
 };
 
-export const Container = ({ children, style }: ContainerProps) => {
-  return (
-    <View
-      style={{
-        ...style,
-        flex: 1,
-        marginHorizontal: 10,
-      }}
-    >
-      {children}
-    </View>
-  );
+export const Container = ({
+  children,
+  style,
+  mode = "top",
+}: ContainerProps) => {
+  const windowHeight = Dimensions.get("window").height;
+
+  const styles = {
+    ...style,
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: mode === "top" ? 0 : (1 / 5) * windowHeight,
+  };
+
+  return <View style={styles}>{children}</View>;
 };
