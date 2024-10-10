1287f2a192b86e8883fce19acf69d34025ff602f
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 16:12:00 +0200

    feat: Add SizeMultiplier for IconButton.tsx and change color to secondary

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,16 +1,27 @@
-import { Button, Icon } from "react-native-paper";
+import { Button, Icon, useTheme } from "react-native-paper";
 import { ImageSourcePropType } from "react-native";
 
 type IconButtonProps = {
   source: ImageSourcePropType | string | undefined;
   onPress: () => void;
+  sizeMultiplier?: number;
 };
 
-export const IconButton = ({ source, onPress }: IconButtonProps) => {
+export const IconButton = ({
+  source,
+  onPress,
+  sizeMultiplier = 2,
+}: IconButtonProps) => {
+  const theme = useTheme();
+
   return (
     <Button
-      icon={({ size, color }) => (
-        <Icon size={2 * size} source={source} color={color}></Icon>
+      icon={({ size }) => (
+        <Icon
+          size={sizeMultiplier * size}
+          source={source}
+          color={theme.colors.secondary}
+        ></Icon>
       )}
       onPress={onPress}
     >
