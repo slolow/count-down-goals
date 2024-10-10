0f12ffb457de8618a1a991e48a07d1c8e7918bcd
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 13:46:19 +0200

    feat: Map goals to Cards in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
 import { Card } from "react-native-paper";
 import { PrimaryText } from "@/components/PrimaryText";
 
+const goals = [
+  "live sugar free",
+  "play guitar for 10 minutes every day",
+  "think about a moment in your day that you are grateful for",
+];
+
 const Index = () => {
-  return (
+  return goals.map((goal) => (
     <Card style={{ margin: 10 }}>
       <Card.Content>
-        <PrimaryText>Card title</PrimaryText>
+        <PrimaryText>{goal}</PrimaryText>
       </Card.Content>
     </Card>
-  );
+  ));
 };
 
 export default Index;
