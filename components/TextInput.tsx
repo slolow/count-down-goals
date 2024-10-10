73e003fd9f9050bc9eaf37d74fe21067ebee1e8b
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 19:55:50 +0200

    feat: Add inputMode prop to TextInput.tsx

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,16 +1,24 @@
 import { TextInput as RNPTextInput, useTheme } from "react-native-paper";
+import { InputModeOptions } from "react-native";
 
 type TextInputProps = {
   label: string;
   value: string;
-  onChangeText: (text: string) => void;
+  onChangeText: (value: string) => void;
+  inputMode?: InputModeOptions | undefined;
 };
 
-export const TextInput = ({ label, value, onChangeText }: TextInputProps) => {
+export const TextInput = ({
+  label,
+  value,
+  onChangeText,
+  inputMode = "text",
+}: TextInputProps) => {
   const theme = useTheme();
 
   return (
     <RNPTextInput
+      inputMode={inputMode}
       label={label}
       value={value}
       mode={"outlined"}
