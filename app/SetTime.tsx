ab3c37e5a96f8f4a2dbe96232c8476f44d0164ee
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-10 19:28:02 +0200

    feat: Implement SetTime.tsx screen

--- app/SetTime.tsx
+++ app/SetTime.tsx
@@ -1,14 +1,27 @@
-import { PrimaryText } from "@/components/PrimaryText";
-import { Link } from "expo-router";
 import { Container } from "@/components/Container";
+import { useState } from "react";
+import { TextInput } from "@/components/TextInput";
+import { TwoButtonsGroup } from "@/components/TwoButtonsGroup";
 
 const SetTime = () => {
+  const [text, setText] = useState("");
+
   return (
-    <Container>
-      <Link href="/Goal">
-        <PrimaryText>See goal</PrimaryText>
-      </Link>
-    </Container>
+    <>
+      <Container mode={"centered"}>
+        <TextInput
+          label={"time"}
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+      </Container>
+      <TwoButtonsGroup
+        linkForLeftButton={"/SetGoal"}
+        linkForRightButton={"/"}
+        textLeftButton={"back"}
+        textRightButton={"start"}
+      />
+    </>
   );
 };
 
