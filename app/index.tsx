7f39ef1a741b47ebecb01bf63a65db621a44d170
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-12 01:46:05 +0200

    feat: Add delete goal logic

--- app/index.tsx
+++ app/index.tsx
@@ -7,14 +7,41 @@ import { Link } from "expo-router";
 import { SPACE_BETWEEN_SCREEN_BOTTOM_AND_LOWER_BUTTONS } from "@/assets/constants/ConstantStyles";
 import { useContext } from "react";
 import { GoalsContext } from "@/providers/GoalsProvider";
-import { type Goals } from "@/data/goals";
+import { type Goal } from "@/data/goals";
+import { Alert } from "react-native";
 
 const GoalsList = () => {
   // @ts-ignore
-  const { getGoals } = useContext(GoalsContext);
-  const goals: Goals = getGoals();
+  const { goals, setGoals } = useContext(GoalsContext);
 
-  return goals.map((goal) => (
+  const handleDeletePress = (goal: Goal) => {
+    Alert.alert(
+      "Are you sure?",
+      "Do you really want to delete this item?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel", // iOS specific, makes the button look bold
+        },
+        {
+          text: "Delete",
+          onPress: () => handleDelete(goal),
+          style: "destructive", // iOS specific, makes the button red
+        },
+      ],
+      { cancelable: false }, // Prevent dismissing the dialog by tapping outside
+    );
+  };
+
+  const handleDelete = (goalToDelete: Goal) => {
+    const updatedGoals = goals.filter(
+      (goal: Goal) => goal.id != goalToDelete.id,
+    );
+    setGoals(updatedGoals);
+  };
+
+  return goals.map((goal: Goal) => (
     <Card
       key={goal.id}
       style={{
@@ -23,7 +50,12 @@ const GoalsList = () => {
     >
       <Card.Title
         title={<PrimaryText>{goal.content}</PrimaryText>}
-        right={() => <IconButton source={"delete"} />}
+        right={() => (
+          <IconButton
+            source={"delete"}
+            onPress={() => handleDeletePress(goal)}
+          />
+        )}
       />
     </Card>
   ));
