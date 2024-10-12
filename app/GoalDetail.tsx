1064864e56720c61d1a795085c26e23ea2320edd
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-12 23:32:28 +0200

    feat: Restyle GoalDetail.tsx screen

--- app/GoalDetail.tsx
+++ app/GoalDetail.tsx
@@ -1,14 +1,16 @@
+import { useTheme } from "react-native-paper";
 import { PrimaryText } from "@/components/PrimaryText";
-import { Container } from "@/components/Container";
 import { useContext } from "react";
 import { GoalsContext } from "@/providers/GoalsProvider";
 import { type Goal } from "@/data/goals";
 import { LinkButton } from "@/components/LinkButton";
 import { MARGIN_VERTICAL } from "@/constants/ConstantStyles";
+import { View } from "react-native";
 
 const GoalDetail = () => {
   const { goals, setGoals } = useContext(GoalsContext)!;
   const goal = goals.find((goal: Goal) => goal.selected)!;
+  const theme = useTheme();
 
   const handleBackPress = () => {
     const updatedGoals = goals.map((goal: Goal) => {
@@ -25,10 +27,10 @@ const GoalDetail = () => {
   };
 
   return (
-    <Container mode={"centered"}>
-      <PrimaryText style={{ marginBottom: MARGIN_VERTICAL }}>
-        {goal.content}
-      </PrimaryText>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    >
+      <PrimaryText variant={"displaySmall"}>{goal.content}</PrimaryText>
       <PrimaryText style={{ marginBottom: MARGIN_VERTICAL }}>
         {goal.reached
           ? `🎉 You reached your goal of ${goal.days} days! 🎉`
@@ -39,8 +41,11 @@ const GoalDetail = () => {
         mode={"contained"}
         onPress={handleBackPress}
         text={"Back"}
+        style={{
+          backgroundColor: theme.colors.secondary,
+        }}
       />
-    </Container>
+    </View>
   );
 };
 
