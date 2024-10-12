12b1de6abc7a646295716bc9dac49ff7d516fba2
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-12 04:24:38 +0200

    feat: Add style prop to PrimaryText.tsx

--- components/PrimaryText.tsx
+++ components/PrimaryText.tsx
@@ -1,17 +1,21 @@
 import { Text } from "react-native-paper";
 import { useTheme } from "react-native-paper";
 import React, { ReactNode } from "react";
+import { StyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
+import { TextStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 
 type PrimaryTextProps = {
   children: ReactNode;
+  style?: StyleProp<TextStyle>;
 };
 
-export const PrimaryText = ({ children }: PrimaryTextProps) => {
+export const PrimaryText = ({ children, style }: PrimaryTextProps) => {
   const theme = useTheme();
 
   return (
     <Text
       style={{
+        ...(style as object),
         fontFamily: "Anton",
         color: theme.colors.primary,
       }}
