3ba194f38883ad107de0a233dcde1cdcf941454c
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-19 22:29:47 +0200

    Feat: Add SignIn.tsx with github logic

--- app/SignIn.tsx
+++ app/SignIn.tsx
@@ -1,8 +1,28 @@
 import { Button, Icon, useTheme } from "react-native-paper";
 import { PrimaryText } from "@/components/PrimaryText";
+import * as WebBrowser from "expo-web-browser";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: `https://github.com/login/oauth/authorize?client_id=${process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID}`,
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint: `https://github.com/settings/connections/applications/${process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID}`,
+};
 
 const SignIn = () => {
   const theme = useTheme();
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID!,
+      scopes: [],
+      redirectUri: makeRedirectUri(),
+    },
+    discovery,
+  );
+
   return (
     <Button
       mode={"contained"}
@@ -14,6 +34,7 @@ const SignIn = () => {
           color={theme.colors.background}
         />
       )}
+      onPress={() => promptAsync()}
     >
       <PrimaryText style={{ color: theme.colors.background }}>
         Sign in with github
