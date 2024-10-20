0c900de70955cc26eeef0a01c06dfec97d8d0ea0
Laszlo Starost <laszlo.starost@barmer.de>
2024-10-20 02:21:52 +0200

    feat: Add getAccessToken logic

--- app/SignIn.tsx
+++ app/SignIn.tsx
@@ -2,6 +2,8 @@ import { Button, Icon, useTheme } from "react-native-paper";
 import { PrimaryText } from "@/components/PrimaryText";
 import * as WebBrowser from "expo-web-browser";
 import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+import { useAuthContext } from "@/hook/useAuthContext";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -13,8 +15,9 @@ const discovery = {
 
 const SignIn = () => {
   const theme = useTheme();
+  const { setAccessToken } = useAuthContext()!;
 
-  const [request, response, promptAsync] = useAuthRequest(
+  const [, response, promptAsync] = useAuthRequest(
     {
       clientId: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID!,
       scopes: [],
@@ -22,6 +25,31 @@ const SignIn = () => {
     },
     discovery,
   );
+
+  const getAccessToken = async (code: string) => {
+    const response = await fetch(
+      "http://localhost:4000/accessToken?code=" + code,
+      {
+        method: "GET",
+      },
+    );
+
+    return await response.json();
+  };
+
+  const handleOAuthResponse = async () => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+      const { access_token } = await getAccessToken(code);
+      setAccessToken(access_token);
+    }
+  };
+
+  useEffect(() => {
+    handleOAuthResponse().catch((error) =>
+      console.error("failed to get access token from proxy", error),
+    );
+  }, [response]);
 
   return (
     <Button
